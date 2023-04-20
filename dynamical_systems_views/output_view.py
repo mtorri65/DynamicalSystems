9a455f2e8fef46a499913c552da75462ae76cf05
mtorri65 <Massimo_torri@hotmail.com>
2023-04-20 01:25:29 -0600

    Fixed Output window refresh problem

--- dynamical_systems_views/output_view.py
+++ dynamical_systems_views/output_view.py
@@ -48,41 +48,4 @@ class OutputView(tk.Frame):
         self.ax = self.fig.add_subplot()
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)  
         self.canvas.get_tk_widget().pack()        
-
-#        self.buttons_frame = tk.Frame(self)
-#        self.buttons_frame.grid(row = 3, column = 0, padx=10, pady=10, sticky='we', columnspan=2)
-#        self.start_button = tk.Button(self.buttons_frame, text= 'Systems', font= (font_parameters))
-#        self.simulations_button = tk.Button(self.buttons_frame, text= 'Simulations', font= (font_parameters))
-#        self.start_button.grid(row=0, column=0, padx=10, pady=10, sticky='wn')
-#        self.simulations_button.grid(row=0, column=1, padx = 10, pady=10, sticky='wn')
-
-#        self.close_button = tk.Button(self.buttons_frame, text = 'Close Window', font = (font_parameters))
-#        self.close_button.grid(row=0, column=2, padx=10, pady=10, sticky='wn')
-
-
-
-
-'''
-        self.graphtype_combo.bind('<<ComboboxSelected>>', comboFunction)
-
-    def comboFunction(self, event):
-        print(self.graphtype_combo.get())
-'''
-
-'''
-        fontsize = str(18)    
-        font_parameters = vc.font + fontsize
-        self.mechanical_system_label = tk.Label(self, padx=10, pady=10, font= (font_parameters))
-
-        fontsize = str(12)    
-        font_parameters = vc.font + fontsize
-        self.create_button = tk.Button(self, text= 'Create a new simulation', font= (font_parameters))
-
-        self.buttons_frame = tk.Frame(self)
-        self.previous_button = tk.Button(self.buttons_frame,text='Previous', font=(font_parameters))
-
-        self.mechanical_system_label.grid(row=0, column=0, padx=10, pady=10, columnspan=2 )
-        self.create_button.grid(row=1, column=0, padx=10, pady=10, sticky='wn')
-        self.buttons_frame.grid(row=2, column=0, padx=10, pady=10, columnspan = 2)
-        self.previous_button.grid(row=0, column=0, padx = 10, pady=10)        
-'''        
+        
